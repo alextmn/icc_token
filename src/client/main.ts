415d3eab1cfe9efb3da671e4f578db6969a8a1c9
alex <alextmn1@gmail.com>
2022-06-14 14:38:24 -0400

    solana connector

--- src/client/main.ts
+++ src/client/main.ts
@@ -119,13 +119,13 @@ app.post('/solana-tx', async (req: Request, res: Response) => {
 
 
 app.listen(3000, async () =>  {
-  // await establishConnection();
+  await establishConnection();
 
-  // // Determine who pays for the fees
-  // await establishPayer();
+  // Determine who pays for the fees
+  await establishPayer();
 
-  // // Check if the program has been deployed
-  // await checkProgram();
+  // Check if the program has been deployed
+  await checkProgram();
   console.log(`[server]: Server is running at http://localhost:3000`);
 
 });
